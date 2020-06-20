657d76c24b017aa7076a5f5644e328ea8dadf52e
Micheleen Harris <michhar@microsoft.com>
2020-06-19 20:22:06 -0700

    Day 1 PyTorch updates and match folders to agenda

--- day1/2.3.AzureMLWithIoT/pytorch_score_iot.py
+++ day1/2.3.AzureMLWithIoT/pytorch_score_iot.py
@@ -34,7 +34,8 @@ def transform_image(input_data):
 def init():
     global model
     # Get a registered model
-    model_path = Model.get_model_path('suspicious-behavior-pytorch')
+    AZUREML_MODEL_DIR = os.getenv('AZUREML_MODEL_DIR', '') # this env var is native to the Azure ML container
+    model_path = os.path.join(AZUREML_MODEL_DIR, 'model_finetuned.pth')
     # Load with pytorch
     model = torch.load(model_path, map_location=lambda storage, loc: storage)
     # Set to evaluation mode
